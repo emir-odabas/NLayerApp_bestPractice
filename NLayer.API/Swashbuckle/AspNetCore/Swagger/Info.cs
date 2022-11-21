5257297ba29f9ded3d62f4c404488bab12a73e3f
emir.odabas <emir.odabas@infinidium.com.tr>
2022-11-21 11:15:40 +0300

    Validator Added

---
+++ NLayer.API/Swashbuckle/AspNetCore/Swagger/Info.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Swashbuckle.AspNetCore.Swagger
+{
+    internal class Info : OpenApiInfo
+    {
+        public string Version { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public OpenApiContact Contact { get; set; }
+        public OpenApiLicense License { get; set; }
+    }
+}
