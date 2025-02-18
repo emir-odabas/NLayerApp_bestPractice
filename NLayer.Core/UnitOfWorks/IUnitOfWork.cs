4b2c8d6911fb7e2d51706c86bdafede7e83a6dbf
Emir Odabas <100259580+emir-odabas@users.noreply.github.com>
2025-02-18 05:48:21 +0300

    Update IUnitOfWork.cs

--- NLayer.Core/UnitOfWorks/IUnitOfWork.cs
+++ NLayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -2,7 +2,6 @@
 {
     public interface IUnitOfWork
     {
-        //Bu design pattern veritabanına yapılacak işlemlerin tek bir transaction(işlem) üzerinden yapılabilmesini sağlıyor 
         Task CommitAsync();
 
         void Commit();
